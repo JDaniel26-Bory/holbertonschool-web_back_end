573035176175e8031865a1f47bc3c0190c2ae22b
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-28 11:12:18 -0500

    add the files

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+import math
+"""function floor returns the floor of the float."""
+
+
+def floor(n: float) -> int:
+    """return a float with its floor"""
+    return math.floor(n)
