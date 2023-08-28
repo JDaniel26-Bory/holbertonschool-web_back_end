babacba6ac1a7414e22922b23e58975b4e2109e9
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-28 11:57:57 -0500

    update the files for one fix

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+"""Import Modules"""
+from typing import List
 """function sum_list"""
 
 
-def sum_list(input_list: list) -> float:
-    """returns their sum as a float."""
+def sum_list(input_list: List[float]) -> float:
+    """return the sum of the list"""
     return sum(input_list)
