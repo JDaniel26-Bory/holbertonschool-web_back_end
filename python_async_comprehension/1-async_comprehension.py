cf5f4ef7374afa820515ac96da8d3c34c31bcc75
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-08-30 14:25:15 -0500

    add the file

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
 import asyncio
+from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """async comprehension"""
     randon_numbers = [randon_num async for randon_num in async_generator()]
     return randon_numbers
