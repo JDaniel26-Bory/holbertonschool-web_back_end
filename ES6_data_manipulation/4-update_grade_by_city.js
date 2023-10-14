8206ed3e7834da3ca6ce040a0aa4a001aa67e84a
JDaniel26-Bory <juanrestrepo246@gmail.com>
2023-10-14 15:37:05 -0500

    add the files

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,21 +7,21 @@ function updateStudentGradeByCity(students, city, newGrades) {
   }
   const stCity = students.filter((student) => student.location === city);
   const studentsGraded = stCity.map((student) => {
-  const gradeFilter = newGrades.filter(
-    (newGrade) => newGrade.studentId === student.id,
-  );
+    const gradeFilter = newGrades.filter(
+      (newGrade) => newGrade.studentId === student.id,
+    );
   
-  let grade;
-  if (gradeFilter[0]) {
-    grade = gradeFilter[0].grade;
-    } else {
-    grade = 'N/A';
+    let grade;
+    if (gradeFilter[0]) {
+      grade = gradeFilter[0].grade;
+      } else {
+      grade = 'N/A';
     }
-  return {
-    ...student,
-    grade,
+    return {
+      ...student,
+      grade,
     };
-});
+  });
   return studentsGraded;
 }
 export default updateStudentGradeByCity;
